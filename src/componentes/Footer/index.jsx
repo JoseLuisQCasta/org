15f28c65c24f9fd11f5c51c994808a0b205fa90d
jose luis quispe <joseluis14j2@gmail.com>
2023-08-25 20:12:02 -0300

    arreglo de footer

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -3,18 +3,18 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.facebook.com/jose.quispe.5059'>
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
             <a href='https://www.aluracursos.com/'>
                 <img src="/img/twitter.png" alt='twitter' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/jose_luis._.98/'>
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
         <img src='/img/Logo.png' alt='org' />
-        <strong>Desarrollado por Alura</strong>
+        <strong>Desarrollado por Jose</strong>
     </footer>
 }
 
